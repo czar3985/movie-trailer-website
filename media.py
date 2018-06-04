5fa675b461368cd4607c99bac837e44640b653f1
Rina Lingad <pixie.czar@gmail.com>
2018-06-04 14:46:11 +1200

    Add docstrings for the Movie class

--- media.py
+++ media.py
@@ -2,13 +2,22 @@ import webbrowser
 
 
 class Movie():
+    """Stored details and actions that can be performed for each movie"""
+
     def __init__(self, title, storyline, poster_image, trailer_video):
-        # Movie object properties to be displayed in the trailer website
+        """Initializes movie object properties
+
+        Args:
+            title (str): Movie title
+            storyline (str): Synopsis of the movie
+            poster_image (str): Link to an image of the movie poster
+            trailer_video (str): Link to a youtube trailer video
+        """
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_video
 
     def show_trailer(self):
-        # Bring up and play the trailer in a new window
+        """Bring up and play the trailer in a new window"""
         webbrowser.open(self.trailer_video)
